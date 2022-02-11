37582e1538782273531a703f119f6770643ee995
Eduardo Marra <eduardomarra@gmail.com>
2022-02-11 15:03:48 -0300

    feat: created theme and configure theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 import App from './App'
+
+import theme from './styles/theme'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+            <GlobalStyles />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 )
